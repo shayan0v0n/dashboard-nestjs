b363d42307c35f3c63142590560c16e1b1d5a322
shayan vosoughi <shayanwqhw@gmail.com>
2022-11-08 21:06:09 +0330

    refactor: :recycle: Create Res Folder For Resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TodosModule } from './todos/todos.module';
-import { UrlsModule } from './urls/urls.module';
-import { PasswordsModule } from './passwords/passwords.module';
+import { TodosModule } from './resource/todos/todos.module';
+import { UrlsModule } from './resource/urls/urls.module';
+import { PasswordsModule } from './resource/passwords/passwords.module';
 
 @Module({
   imports: [
--- src/resource/passwords/passwords.service.ts
+++ src/resource/passwords/passwords.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Password, passwordDocument } from 'src/schema/passwords.schma';
-import { CreateUrlDto } from 'src/urls/urls.dto';
+import { CreateUrlDto } from 'src/resource/urls/urls.dto';
 import { CreatePasswordDto, UpdatePasswordDto } from './passwords.dto';
 
 @Injectable()
